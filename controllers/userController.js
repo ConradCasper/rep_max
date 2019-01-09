987e27e018b261c2a8d466e9431aeb2e6ac78508
conrad casper <cnrdcasper@gmail.com>
2019-01-09 16:04:57 -0500

    create view is rendering, working on routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,20 @@ const userController = {
             console.log(user)
             res.render('user/index', { user: user })
         })
+    },
+    new: (req, res) => {
+        res.render('user/new')
+    },
+    create: (req, res) => {
+        User.create({
+        name: req.body.name,
+        age: req.body.age,
+        weight: req.body.weight
+        }).then(newUser =>{
+            res.redirect('/')
+        })
     }
+
 }
 
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ const BodyAreaController = require('../controllers/BodyAreaController')
 const userController = require('../controllers/userController')
 
 
-router.get('/users', userController.index)
+router.get('/', userController.index)
+router.post('/', userController.create)
+router.get('/new', userController.new)
+// router.get('/:id', linkController.show)
+// router.get('/:id/edit', linkController.edit)
+// router.patch('/:id', linkController.update)
+// router.delete('/:id', linkController.delete)
+
+
+
 router.get('/bodyarea', BodyAreaController.index)
 
 module.exports = router
