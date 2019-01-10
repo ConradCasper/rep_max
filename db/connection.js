35da560eb4bd2465865ae3cb5c24de9600095d99
conrad casper <cnrdcasper@gmail.com>
2019-01-09 19:40:23 -0500

    setting up heroku

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,14 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost/rep-max").then(() => {
-    console.log("MONGODB is now connected")
-})
+// mongoose.connect("mongodb://localhost/rep-max").then(() => {
+//     console.log("MONGODB is now connected")
+// })
 
-module.exports = mongoose;
+// module.exports = mongoose;
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB')
+}) 
+
+module.exports = mongoose
